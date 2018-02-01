af60bfe3e3b8f022cc9150a45fd20c3a6ea2b2d5
Matt McCormick <matt.mccormick@kitware.com>
2018-02-01 14:36:32 -0500

    Remove rendering components from MedicalDemo1

--- MedicalDemo1/MedicalDemo1.cxx
+++ MedicalDemo1/MedicalDemo1.cxx
@@ -3,60 +3,18 @@
 // represents the skin and displays it.
 //
 
-#include <vtkActor.h>
-#include <vtkCamera.h>
 #include <vtkMarchingCubes.h>
 #include <vtkMetaImageReader.h>
-#include <vtkNamedColors.h>
-#include <vtkOutlineFilter.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkProperty.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
 #include <vtkSmartPointer.h>
-
-#include <algorithm>
-#include <array>
+#include <vtkXMLPolyDataWriter.h>
 
 int main (int argc, char *argv[])
 {
-  if (argc < 2)
+  if (argc < 3)
   {
-    cout << "Usage: " << argv[0] << " file.mhd" << endl;
+    cout << "Usage: " << argv[0] << " input.mhd output.vtp" << endl;
     return EXIT_FAILURE;
   }
-
-  vtkSmartPointer<vtkNamedColors> colors =
-    vtkSmartPointer<vtkNamedColors>::New();
-
-  // Set the colors.
-  auto SetColor = [&colors](std::array<double, 3>& v,
-                            std::string const& colorName) {
-    auto const scaleFactor = 255.0;
-    std::transform(std::begin(v), std::end(v), std::begin(v),
-                   [=](double const& n) { return n / scaleFactor; });
-    colors->SetColor(colorName, v.data());
-    return;
-  };
-  std::array<double, 3> skinColor{{255, 125, 64}};
-  SetColor(skinColor, "SkinColor");
-  std::array<double, 3> bkgColor{{51, 77, 102}};
-  SetColor(bkgColor, "BkgColor");
-
-  // Create the renderer, the render window, and the interactor. The renderer
-  // draws into the render window, the interactor enables mouse- and
-  // keyboard-based interaction with the data within the render window.
-  //
-  vtkSmartPointer<vtkRenderer> aRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renWin =
-    vtkSmartPointer<vtkRenderWindow>::New();
-  renWin->AddRenderer(aRenderer);
-
-  vtkSmartPointer<vtkRenderWindowInteractor> iren =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  iren->SetRenderWindow(renWin);
 
   vtkSmartPointer<vtkMetaImageReader> reader =
     vtkSmartPointer<vtkMetaImageReader>::New();
@@ -69,69 +27,11 @@ int main (int argc, char *argv[])
   skinExtractor->SetInputConnection(reader->GetOutputPort());
   skinExtractor->SetValue(0, 500);
 
-  vtkSmartPointer<vtkPolyDataMapper> skinMapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
-  skinMapper->SetInputConnection(skinExtractor->GetOutputPort());
-  skinMapper->ScalarVisibilityOff();
-
-  vtkSmartPointer<vtkActor> skin =
-    vtkSmartPointer<vtkActor>::New();
-  skin->SetMapper(skinMapper);
-  skin->GetProperty()->SetDiffuseColor(colors->GetColor3d("SkinColor").GetData());
-
-  // An outline provides context around the data.
-  //
-  vtkSmartPointer<vtkOutlineFilter> outlineData =
-    vtkSmartPointer<vtkOutlineFilter>::New();
-  outlineData->SetInputConnection(reader->GetOutputPort());
-
-  vtkSmartPointer<vtkPolyDataMapper> mapOutline =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
-  mapOutline->SetInputConnection(outlineData->GetOutputPort());
-
-  vtkSmartPointer<vtkActor> outline =
-    vtkSmartPointer<vtkActor>::New();
-  outline->SetMapper(mapOutline);
-  outline->GetProperty()->SetColor(colors->GetColor3d("Black").GetData());
-
-  // It is convenient to create an initial view of the data. The FocalPoint
-  // and Position form a vector direction. Later on (ResetCamera() method)
-  // this vector is used to position the camera to look at the data in
-  // this direction.
-  vtkSmartPointer<vtkCamera> aCamera =
-    vtkSmartPointer<vtkCamera>::New();
-  aCamera->SetViewUp (0, 0, -1);
-  aCamera->SetPosition (0, -1, 0);
-  aCamera->SetFocalPoint (0, 0, 0);
-  aCamera->ComputeViewPlaneNormal();
-  aCamera->Azimuth(30.0);
-  aCamera->Elevation(30.0);
-
-  // Actors are added to the renderer. An initial camera view is created.
-  // The Dolly() method moves the camera towards the FocalPoint,
-  // thereby enlarging the image.
-  aRenderer->AddActor(outline);
-  aRenderer->AddActor(skin);
-  aRenderer->SetActiveCamera(aCamera);
-  aRenderer->ResetCamera ();
-  aCamera->Dolly(1.5);
-
-  // Set a background color for the renderer and set the size of the
-  // render window (expressed in pixels).
-  aRenderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
-  renWin->SetSize(640, 480);
-
-  // Note that when camera movement occurs (as it does in the Dolly()
-  // method), the clipping planes often need adjusting. Clipping planes
-  // consist of two planes: near and far along the view direction. The
-  // near plane clips out objects in front of the plane; the far plane
-  // clips out objects behind the plane. This way only what is drawn
-  // between the planes is actually rendered.
-  aRenderer->ResetCameraClippingRange ();
-
-  // Initialize the event loop and then start it.
-  iren->Initialize();
-  iren->Start();
+  vtkSmartPointer<vtkXMLPolyDataWriter> writer =
+    vtkSmartPointer<vtkXMLPolyDataWriter>::New();
+  writer->SetInputConnection(skinExtractor->GetOutputPort());
+  writer->SetFileName(argv[2]);
+  writer->Write();
 
   return EXIT_SUCCESS;
 }
