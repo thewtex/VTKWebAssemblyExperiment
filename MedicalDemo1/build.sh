a4c110b76745978b94e85ffd8811fe28443b9453
Matt McCormick <matt.mccormick@kitware.com>
2018-02-01 14:22:08 -0500

    Make sure to build with Release CMAKE_BUILD_TYPE

--- MedicalDemo1/build.sh
+++ MedicalDemo1/build.sh
@@ -7,8 +7,9 @@ docker_image=kitware/itk-js-vtk:latest
 
 dockcross=./build/dockcross
 
+mkdir -p ./build
 docker run --rm $docker_image > $dockcross
 chmod +x $dockcross
 
-$dockcross cmake -Bbuild -H. -GNinja -DVTK_DIR=/VTK-build
+$dockcross cmake -Bbuild -H. -GNinja -DVTK_DIR=/VTK-build -DCMAKE_BUILD_TYPE=Release
 $dockcross ninja -Cbuild
